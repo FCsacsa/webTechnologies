a889bd49e5ba1daf33948111c45ed910e60f7253
Csanad Farkas <farkas.csacsa@gmail.com>
2021-01-12 14:20:36 +0100

    grid added to make the board 'always' correct

--- HEX/public/javascripts/game.js
+++ HEX/public/javascripts/game.js
@@ -1,7 +1,8 @@
 "use strict";
-let board_start_x = 5;
+let board_start_x = 2;
 let board_start_y = 2;
-
+let red = "red";
+let blue = "blue";
 
 //loading the table
 let grid = document.getElementById("grid");
@@ -14,8 +15,46 @@ for (let i = 0; i < 11; i++){
         //console.log(tile.childNodes);
         hexagon.style = "pointer-events: all;";
         hexagon.addEventListener("click", ()=>{
-            clickHandler(j, i);
+            clickHandler(j, i, hexagon);
         });
+        let border;
+        switch (i) {
+            case 10:
+                border = document.createElementNS("http://www.w3.org/2000/svg", "polyline");
+                border.style = `stroke: ${blue}; stroke-width: 5; fill:none;`;
+                border.setAttribute("points", "0,51.96 30,69.28 60,51.96");
+                tile.appendChild(border);
+                break;
+            
+            case 0:
+                border = document.createElementNS("http://www.w3.org/2000/svg", "polyline");
+                border.style = `stroke: ${blue}; stroke-width: 5; fill:none;`;
+                border.setAttribute("points", "0,17.32 30,0 60,17.32");
+                tile.appendChild(border);
+                break;
+        
+            default:
+                break;
+        }
+        let border2;
+        switch (j) {
+            case 10:
+                border = document.createElementNS("http://www.w3.org/2000/svg", "polyline");
+                border.style = `stroke: ${red}; stroke-width: 5; fill:none;`;
+                border.setAttribute("points", "60,51.96 60,17.32 30,0");
+                tile.appendChild(border);
+                break;
+            
+            case 0:
+                border = document.createElementNS("http://www.w3.org/2000/svg", "polyline");
+                border.style = `stroke: ${red}; stroke-width: 5; fill:none;`;
+                border.setAttribute("points", "0,17.32 0,51.96 30,69.28");
+                tile.appendChild(border);
+                break;
+        
+            default:
+                break;
+        }
         tile.style.gridColumnStart = i + (j * 2) + board_start_x;
         tile.style.gridColumnEnd = i + (j * 2) + board_start_x + 2;
         tile.style.gridRowStart = (i * 2) + board_start_y;
@@ -26,8 +65,10 @@ for (let i = 0; i < 11; i++){
 }
 
 let home = document.getElementById("home");
-home.addEventListener("click", () => {clickHandler(10, 9)});
+home.addEventListener("click", () => {location.reload()});
 
-function clickHandler(x,y) {
-    alert(`Button clicked! ${x}, ${y}`);
+function clickHandler(x,y, th) {
+    console.log(`Button clicked! ${x}, ${y}`);
+    let color = Math.floor(Math.random()*16777215).toString(16);
+    th.style = `fill: ${blue}`
 }
